8542fab58afb29f50773e31b26bc9439c2e09087
IvanVoile <723909978@qq.com>
2016-03-02 14:38:50 +0800

    Initial Commit

---
+++ PersonalHealth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonalHealth
+//
+//  Created by lifan on 16/3/2.
+//  Copyright © 2016年 PHTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersonalHealth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PersonalHealth
+//
+//  Created by lifan on 16/3/2.
+//  Copyright © 2016年 PHTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
